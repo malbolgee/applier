5c3eddce23fa94dc833c111d76e059447772a21c
Malbolge <victor.gomes@icomp.ufam.edu.br>
2024-11-10 12:02:42 -0400

    misc: Adjuste documentation, identation and other things
    This commit does several things:
    
    - Adjust the line length from 88 to 119 before it gets wrapped.
    - Give propper typping hints to methods and variables.
    - Add some important checks before dealing with variables that may be
      None or empty.
    - Refactor the _request method to handle if the response is valid or
      not.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- applier.py
+++ applier.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 
 from requests.auth import HTTPBasicAuth
+from threading import Thread
+from argparse import Namespace
+from typing import Optional
 import argparse
 import json
 import os
 import re
 import requests
 import subprocess
-import threading
 
 
 class GerritConnectionError(Exception):
     pass
 
 
-def arg_parse():
+def arg_parse() -> Namespace:
     parser = argparse.ArgumentParser(
         prog="applier",
         description="Automatically apply cherry-picks to multiple repos at the same time.",
@@ -35,7 +37,7 @@ class Args:
     _OPTIONS_USERNAME_DEFAULT = {
         "type": str,
         "default": os.getenv("GERRIT_USERNAME"),
-        "help": "Set a Gerrit user to use with this session. Otherwise, export a GERRIT_USER enviroment variable.",
+        "help": "Set a Gerrit user to use with this session. Otherwise, export a GERRIT_USERNAME enviroment variable.",
     }
 
     _OPTIONS_PASSWORD_DEFAULT = {
@@ -52,7 +54,7 @@ class Args:
 
     _OPTIONS_AOSP_PATH_DEFAULT = {
         "type": str,
-        "help": "The absolute path of the AOSP root. If not set, execute this program from the root.",
+        "help": "The absolute path of the AOSP root. If not set, execute this program from the AOSP root.",
     }
 
     _OPTIONS_THREADS_DEFAULT = {
@@ -71,16 +73,16 @@ class Args:
 
     _parser = None
 
-    def __init__(self, parser):
+    def __init__(self, parser: Namespace):
         self._set_parser(parser)
 
-    def _set_parser(self, parser) -> None:
+    def _set_parser(self, parser: Namespace) -> None:
         if parser is None:
             raise ValueError("Parser parameter cannot be None")
 
         self._parser = parser
 
-    def _get_parser(self):
+    def _get_parser(self) -> Namespace:
         return self._parser
 
     def parse(self):
@@ -94,20 +96,20 @@ class Args:
 
 class Applier:
 
-    _connector = None
+    _connector: Optional["GerritConnector"] = None
 
-    def __init__(self, parser):
-        self._parser = parser
+    def __init__(self, parser: Namespace):
+        self._parser: Namespace = parser
         self._set_connector()
-        self._filepath = self._parser.filepath
-        self._new_branch = self._parser.new_branch
-        self._use_threads = self._parser.no_threads
-        self._aosp_path = self._parser.aosp_path
+        self._filepath: str = self._parser.filepath
+        self._new_branch: Optional[str] = self._parser.new_branch
+        self._use_threads: bool = self._parser.no_threads
+        self._aosp_path: Optional[str] = self._parser.aosp_path
 
     def _set_connector(self):
         if self._parser.username is None or self._parser.password is None:
             raise ValueError(
-                "Nor username or password can be null. Please export the enviroment variables or pass via arguments."
+                "Nor username or password cannot be None. Please export the enviroment variables or pass via arguments."
             )
 
         self._connector = GerritConnector(self._parser.username, self._parser.password)
@@ -135,19 +137,22 @@ class Applier:
         change_id = re.findall(r"(\d+)", url)[0]
         cp_command = self._get_connector().get_cherry_pick_command(change_id)
 
-        commands = re.findall(r"(?:\w+ \w+) ([^ ]+) ([^ ]+)", cp_command)[0]
-        cp_url, refs = commands
+        cp_url, refs = re.findall(r"(?:\w+ \w+) ([^ ]+) ([^ ]+)", cp_command)[0]
 
         path = self._get_aosp_path() + self._extract_path(cp_url)
 
-        if self._new_branch is not None:
+        #! TODO: do not create branch if it is already created
+        if self._new_branch is not None and len(self._new_branch) > 0:
             self._run_command(self._build_new_branch_command(path, self._new_branch))
 
         self._run_command(self._build_cherry_pick_command(path, cp_url, refs))
 
-    def _get_aosp_path(self):
+    def _get_aosp_path(self) -> str:
         if self._aosp_path is not None:
-            return self._aosp_path
+            if self._aosp_path.endswith("/"):
+                return self._aosp_path
+            else:
+                return self._aosp_path + "/"
 
         return ""
 
@@ -155,7 +160,7 @@ class Applier:
         threads = []
         for url in urls:
             if len(url) > 0:
-                threads.append(threading.Thread(target=self._apply_individual, args=(url,)))
+                threads.append(Thread(target=self._apply_individual, args=(url,)))
 
         for thread in threads:
             thread.start()
@@ -165,14 +170,13 @@ class Applier:
 
     def _run_without_threads(self, urls: str) -> None:
         for url in urls:
-            self._apply_individual(url)
+            if len(url) > 0:
+                self._apply_individual(url)
 
     def _build_cherry_pick_command(self, path: str, url: str, refs: str) -> str:
-        return (
-            f"git -C {path} fetch {url} {refs} && git -C {path} cherry-pick FETCH_HEAD"
-        )
+        return f"git -C {path} fetch {url} {refs} && git -C {path} cherry-pick FETCH_HEAD"
 
-    def _build_new_branch_command(self, path, branch_name):
+    def _build_new_branch_command(self, path: str, branch_name: str) -> str:
         return f"git -C {path} checkout -b {branch_name}"
 
     def _run_command(self, command: str) -> None:
@@ -188,27 +192,23 @@ class GerritConnector:
         self.password = password
 
     def _request(self, change_id: str):
-        return requests.get(
+        response = requests.get(
             self._url.replace("CHANGE_ID", change_id),
             auth=HTTPBasicAuth(self.user, self.password),
         )
 
+        if response.status_code == 200:
+            return response
+
+        raise GerritConnectionError(f"Failed to access Gerrit API. Status code: {response.status_code}")
+
     def get_cherry_pick_command(self, change_id: str) -> str:
         response = self._request(change_id)
 
-        if response.status_code == 200:
-            response_text = (
-                response.text[5:] if response.text.startswith(")]}'") else response.text
-            )
-            response_json = json.loads(response_text)[0]
-            current_revision = response_json["current_revision"]
-            return response_json["revisions"][f"{current_revision}"]["fetch"]["ssh"][
-                "commands"
-            ]["Cherry Pick"]
-        else:
-            raise GerritConnectionError(
-                f"Failed to access Gerrit API. Status code: {response.status_code}"
-            )
+        response_text = response.text[5:] if response.text.startswith(")]}'") else response.text
+        response_json = json.loads(response_text)[0]
+        current_revision = response_json["current_revision"]
+        return response_json["revisions"][current_revision]["fetch"]["ssh"]["commands"]["Cherry Pick"]
 
 
 if __name__ == "__main__":
